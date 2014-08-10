72be1592dfbcd3c5612475e3d0b3106ef29f24c9
Tuomo Hartikainen <tth@harski.org>
2014-08-10 13:12:36 +0300

    Fix error.h header guard

--- error.h
+++ error.h
@@ -1,8 +1,8 @@
-/* Copyright 2013 Tuomo Hartikainen <tth@harski.org>.
+/* Copyright 2013-2014 Tuomo Hartikainen <tth@harski.org>.
  * Licensed under the 2-clause BSD license, see LICENSE for details. */
 
-#ifndef ERROR_H
-#define ERROR_H
+#ifndef PW_ERROR_H
+#define PW_ERROR_H
 
 enum Error {
 	E_SUCCESS,
