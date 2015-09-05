17a2685959115dafb25c4bbd337e0b139757c1ed
Tuomo Hartikainen <tth@harski.org>
2015-09-05 14:42:56 +0300

    Add is_whitespace()

--- strutil.c
+++ strutil.c
@@ -1,14 +1,29 @@
-/* Copyright 2014 Tuomo Hartikainen <tth@harski.org>.
+/* Copyright 2014-2015 Tuomo Hartikainen <tth@harski.org>.
  * Licensed under the 2-clause BSD license, see LICENSE for details. */
 
 #include <errno.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "error.h"
 #include "go.h"
 #include "strutil.h"
+
+#define WHITESPACE "\t\n "
+
+bool is_whitespace (const char c)
+{
+    int i = 0;
+    while (WHITESPACE[i++] != '\0') {
+        if (c == WHITESPACE[i])
+            return true;
+    }
+
+    return false;
+}
+
 
 int strtou (const char * const str, unsigned * restrict u)
 {
--- strutil.h
+++ strutil.h
@@ -1,9 +1,15 @@
-/* Copyright 2014 Tuomo Hartikainen <tth@harski.org>.
+/* Copyright 2014-2015 Tuomo Hartikainen <tth@harski.org>.
  * Licensed under the 2-clause BSD license, see LICENSE for details. */
 
 #ifndef PW_STRUTIL
 #define PW_STRUTIL
 
+#include <stdbool.h>
+
+/* check if char c is \t, \n or ' ' */
+bool is_whitespace (const char c);
+
+/* parse str to unsigned int */
 int strtou (const char * const str, unsigned * restrict u);
 
 #endif
