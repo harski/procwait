864da66cbd0dffddb05fb91cd4b70508d24949d9
Tuomo Hartikainen <tth@harski.org>
2017-02-06 13:09:24 +0200

    Describe public functions in header files

--- fileutil.h
+++ fileutil.h
@@ -15,12 +15,24 @@ struct file {
 
 SLIST_HEAD(filelist, file);
 
+/* deep free struct file */
 void file_destroy (struct file * f);
+
+/* filter out and free all non-numeric /proc -dirs */
 void filter_numeric_dirs (struct filelist * filelist);
+
+/* find PIDs for process pname from filelist, and put the matching processes in
+ * proclist */
 int find_pid (struct filelist * filelist, const char * pname,
 	      struct proclist * proclist);
+
+/* get files in a directory */
 int get_dir_contents (const char * dirpath, struct filelist * filelist);
+
+/* get directies in a directory */
 int get_dir_dirs (const char * dirpath, struct filelist * filelist);
+
+/* get only numeric directories inside /proc */
 int get_proc_dirs (struct filelist * fl);
 
 #endif /* PW_FILEUTIL_H */
--- proc.h
+++ proc.h
@@ -20,9 +20,17 @@ struct proc {
 
 SLIST_HEAD(proclist, proc);
 
+/* read stat file identified by path and parse it to p */
 int parse_stat_file (const char * path, struct proc * p);
+
+/* read stat file identified by PID and parse it to p */
 int parse_stat_pid (const unsigned pid, struct proc * restrict p);
+
+/*  check if p1 and p2 are the same process */
 bool proc_eq (const struct proc * const p1, const struct proc * const p2);
+
+/* check that struct proc is a valid process (i.e. parsing stat file was
+ * succesfull) */
 bool validate_proc (const struct proc * const p);
 
 #endif
